4affd17037fefd94527ee26de0a8832fd4f4eb9e
karmajunkie <keith.gaddis@gmail.com>
2014-06-03 01:04:13 -0500

    Getting a real error from the test (AuthenticationError)

--- lib/authenticator.rb
+++ lib/authenticator.rb
@@ -1,11 +1,17 @@
+require 'authentication_error'
 class Authenticator
   def self.clear!
 
   end
 
+  #my totally secure login method
   def self.sign_in(login, password)
-    raise AuthenticationError.new unless @registrations[login] == password
+    raise AuthenticationError.new unless registrations[login] == password
+  end
 
+  private
+  def self.registrations
+    @registrations ||= Hash.new
   end
 
 end
